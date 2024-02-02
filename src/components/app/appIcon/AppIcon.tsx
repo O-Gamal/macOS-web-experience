7c690b797ad0d37f5d2f5e31b4bda40052b9b3da
Omar Gamal <o.gamal.abdulzaher@gmail.com>
2024-02-02 22:28:05 +0200

    feat: implement dock app tooltips

--- src/components/app/appIcon/AppIcon.tsx
+++ src/components/app/appIcon/AppIcon.tsx
@@ -1,4 +1,5 @@
 import appIcons, { AppIconType } from '@assets/appIcons';
+import Tooltip from '@components/ui/Tooltip';
 
 type AppIconProps = {
   name: AppIconType;
@@ -9,11 +10,13 @@ type AppIconProps = {
 const AppIcon = ({ name, size = 1, isOpen = false }: AppIconProps) => {
   return (
     <div className='relative '>
-      <img
-        src={appIcons[name]}
-        alt={name}
-        style={{ width: `${size * 50}px` }}
-      />
+      <Tooltip content={name}>
+        <img
+          src={appIcons[name]}
+          alt={name}
+          style={{ width: `${size * 50}px` }}
+        />
+      </Tooltip>
       {isOpen && (
         <div className='absolute -bottom-1 left-1/2 h-[3px] w-[3px] -translate-x-1/2 rounded-full bg-white' />
       )}
